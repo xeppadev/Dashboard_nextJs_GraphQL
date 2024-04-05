d891844b30f15648eaf95b2f4d9ea5250c3934f6
Cristhian Rodriguez <cprodriguez@unitru.edu.pe>
2024-04-05 10:56:00 -0500

    add console.log

--- src/models/buscarClienteModel.tsx
+++ src/models/buscarClienteModel.tsx
@@ -1,11 +1,19 @@
 import { getClient } from "@/lib/client";
 import { BUSCAR_CLIENTES_SEARCH } from "../repositories/clientesRepo";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/options";
 
 export async function buscarClientesModel(nombreCliente: string, page: number) {
   const client = getClient();
-
+  const session = await getServerSession(options);
+  console.log(session);
   const { data, loading } = await client.query({
     query: BUSCAR_CLIENTES_SEARCH,
+    context: {
+      headers: {
+        Authorization: `Bearer ${session?.access_token}`,
+      },
+    },
     variables: {
       nombreCliente,
       page,
