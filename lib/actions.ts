d60fa467b9aeb997cfa8d13ffb782cf72630664a
Cristhian Rodriguez <cprodriguez@unitru.edu.pe>
2024-04-10 12:13:50 -0500

    add url to upload file

--- lib/actions.ts
+++ lib/actions.ts
@@ -18,16 +18,17 @@ import { revalidatePath } from "next/cache";
 import { getServerSession } from "next-auth";
 import { options } from "@/app/options";
 import * as z from "zod";
+import { Backend_URL } from "./contants";
 
 type queries = {
   query1: string;
   query2: string | undefined;
 };
-const apiURL = process.env.NEXT_API_URL;
+// const apiURL = process.env.NEXT_API_URL;
 
 export async function sentToExternalAPI(formData: FormData, queries: queries) {
   const session = await getServerSession(options);
-  const url = new URL(`${apiURL}/documentos/upload`);
+  const url = new URL(`${Backend_URL}/documentos/upload`);
   url.searchParams.append("query1", queries.query1);
   if (queries.query2) {
     url.searchParams.append("query2", queries.query2);
